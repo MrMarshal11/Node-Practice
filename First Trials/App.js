feedb9f49c1fda6f26a55ff22bdb4cbf53c755c8
Mr.Marshal <marshalrock11@gmail.com>
2024-11-19 13:09:50 +1000

    Demo using file system

--- First Trials/App.js
+++ First Trials/App.js
@@ -1,6 +1,6 @@
 const { createServer } = require('node:http');
 
-const hostname = '127.4.3.0';
+const hostname = '127.0.0.1';
 const port = 3927;
 
 const server = createServer((req, res) => {
@@ -12,3 +12,16 @@ const server = createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+const fs = require('fs');
+
+const content = 'Here is some dummy content for node trials';
+
+fs.appendFile('./demo.txt', content, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('File written successfully');
+    }
+  });
+  
