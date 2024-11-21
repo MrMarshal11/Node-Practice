a620401bdd2be2d2eaee97a77c292145f0aad5d6
Mr.Marshal <marshalrock11@gmail.com>
2024-11-21 15:28:52 +1000

    Using express routers for the first time

--- 12 Part Series/app.js
+++ 12 Part Series/app.js
@@ -1,22 +1,23 @@
 import express from "express";
+import indexRouter from "./routes/indexRouter.js";
+import aboutRouter from "./routes/aboutRouter.js";
+import contactRouter from "./routes/contactRouter.js";
 
+const PORT = 3000;
 const app = express();
 
-app.listen(3000, () => console.log(`operational under http://localhost:3000`));
+app.listen(PORT, () => console.log(`operational under http://localhost:${PORT}`));
 
-app.get('/', (req, res) => {
-    res.sendFile('/index.html', {root:'htmls'});
-});
+// Index routes
+app.use('/', indexRouter);
 
-app.get('/about', (req, res) => {
-    res.sendFile('/about.html', {root:'htmls'});
-});
+// About routes
+app.use('/about', aboutRouter);
 
-app.get('/contact-us', (req, res) => {
-    res.sendFile('/contact-us.html', {root:'htmls'});
-});
+// Contact-us routes
+app.use('/contact-us', contactRouter);
 
-// If all other roots fail, then signal 404
+// If all other routes fail, then signal 404
 app.use((req, res) => {
     res.status(404).sendFile('/404.html', {root:'htmls'});
 });
