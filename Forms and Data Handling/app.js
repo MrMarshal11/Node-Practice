d69818f337eac6c8e1f7e94ee579067445d606da
Mr.Marshal <marshalrock11@gmail.com>
2024-11-22 18:00:52 +1000

    Forms and Data Handling Follow Along

---
+++ Forms and Data Handling/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import usersRouter from "./routes/usersRouter.js";
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use("/", usersRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!, click here -> http://localhost:3000/`));
