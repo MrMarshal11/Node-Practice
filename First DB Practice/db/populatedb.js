140d388b520107a57294b921f32512562f60e2aa
Mr.Marshal <marshalrock11@gmail.com>
2024-11-24 20:32:54 +1000

    Using a js file to populate a db instead of using psql in the terminal - (readability and efficiency is way better)

---
+++ First DB Practice/db/populatedb.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Client } = pkg;
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS usernames (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  username VARCHAR ( 255 )
+);
+
+INSERT INTO usernames (username) 
+VALUES
+  ('Bryan'),
+  ('Odin'),
+  ('Damon');
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
