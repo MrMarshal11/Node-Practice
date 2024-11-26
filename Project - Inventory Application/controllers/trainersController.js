a9dc441971cedb4be51192bd2768ca134876836d
Mr.Marshal <marshalrock11@gmail.com>
2024-11-27 02:17:49 +1000

    Added nice UX to /teams

--- Project - Inventory Application/controllers/trainersController.js
+++ Project - Inventory Application/controllers/trainersController.js
@@ -1,8 +1,8 @@
-import displayTrainers from "../models/displayValues.js";
+import models from "../models/displayValues.js";
 
 async function trainersRender(req, res) {
     try {
-        const trainers = await displayTrainers();
+        const trainers = await models.displayTrainers();
         res.render("trainers", {trainers: trainers});
     } catch (error) {
         console.log('error in trainersRender()');
--- Project - Inventory Application/models/displayValues.js
+++ Project - Inventory Application/models/displayValues.js
@@ -1,13 +1,28 @@
 import pool from "../db/pool.js";
 
 async function displayTrainers() {
-    try {
-        const { rows } = await pool.query('SELECT trainer FROM trainers');
-        return rows;        
-    } catch (error) {
-        console.log('error at displayTrainers()');
-        console.log(error);
-    }
+  try {
+    const { rows } = await pool.query("SELECT trainer FROM trainers");
+    return rows;
+  } catch (error) {
+    console.log("error at displayTrainers()");
+    console.log(error);
+  }
 }
 
-export default displayTrainers;
+async function displayTeams() {
+  try {
+    console.log("success");
+    // SELECT teams.team_name, pokemon.name
+    // FROM teams
+    // LEFT JOIN team_pokemon
+    // ON teams.id = team_pokemon.team_id
+    // LEFT JOIN pokemon
+    // ON team_pokemon.pokemon_id = pokemon.id;
+  } catch (error) {
+    console.log("error in displayTeams()");
+    console.log(error);
+  }
+}
+
+export default {displayTrainers, displayTeams};
