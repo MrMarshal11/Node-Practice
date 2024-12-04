31797d1e801d3c9cb128c0c3d64f46aa34aec6b1
Mr.Marshal <marshalrock11@gmail.com>
2024-12-05 08:46:38 +1000

    Initial form routes

--- Project - File Uploader/controllers/indexController.js
+++ Project - File Uploader/controllers/indexController.js
@@ -3,10 +3,26 @@ import model from "../prisma/queries.js";
 async function renderIndex(req, res) {
   try {
     // await model.createUserQuery("greg2", "greg2'sPassword"); // just a demo of adding the user to the db
-    await res.render("index");
+    await res.render("index", { page: "home" });
   } catch (error) {
     console.log(`error at renderIndex(), ${error}`);
   }
 }
 
-export default { renderIndex };
+async function renderLogin(req, res) {
+  try {
+    await res.render("index", { page: "login" });
+  } catch (error) {
+    console.log(`error at renderLogin(), ${error}`);
+  }
+}
+
+async function renderSignUp(req, res) {
+  try {
+    await res.render("index", { page: "signUp" });
+  } catch (error) {
+    console.log(`error at renderSIgnUp(), ${error}`);
+  }
+}
+
+export default { renderIndex, renderLogin, renderSignUp };
--- Project - File Uploader/routes/indexRoute.js
+++ Project - File Uploader/routes/indexRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
-import controller from "../controllers/indexController.js"
+import controller from "../controllers/indexController.js";
 
 const indexRouter = express.Router();
 
 indexRouter.get("/", controller.renderIndex);
+indexRouter.get("/login", controller.renderLogin);
+indexRouter.get("/signUp", controller.renderSignUp);
 
 export default indexRouter;
