741c4984a313a941f51ae7edf3532947ef1fc030
Mr.Marshal <marshalrock11@gmail.com>
2024-12-09 21:11:12 +1000

    Initial commit

---
+++ Project - Blog API/src/components/App.jsx
@@ -0,0 +1,13 @@
+import Nav from "./nav";
+import Posts from "./posts";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Posts />
+    </>
+  );
+}
+
+export default App;
---
+++ Project - Blog API/src/components/posts.jsx
@@ -0,0 +1,9 @@
+function Posts() {
+  return (
+    <div>
+      <p>posts will be here etc.</p>
+    </div>
+  );
+}
+
+export default Posts;
