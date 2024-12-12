b774377a6a765d33bdb7229862c16e8346c33216
Mr.Marshal <marshalrock11@gmail.com>
2024-12-12 22:12:18 +1000

    testing ....

--- Project Blog API/backend/app.js
+++ Project Blog API/backend/app.js
@@ -5,12 +5,14 @@ import cors from "cors";
 import indexRouter from "./routes/indexRoutes.js";
 import middleware from "./middleware/passport.js";
 
+// import prisma from "./prisma.js";
+
 const corsOptions = {
   origin: [
     "http://localhost:1000",
     "http://localhost:2000",
-    "https://blogapifrontend1.vercel.app/",
-    "https://blogapifrontend2.vercel.app/",
+    "https://blogapifrontend1.vercel.app",
+    "https://blogapifrontend2.vercel.app",
   ],
 };
 
@@ -22,6 +24,16 @@ middleware.usePassport(app);
 
 app.use(express.json());
 app.use(cors(corsOptions));
+
+// app.get("/testdb", async (req, res) => {
+//   try {
+//     const result = await prisma.query("SELECT NOW() AS current_time");
+//     res.json({ success: true, data: result.rows[0] });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ success: false, error: error.message });
+//   }
+// });
 
 app.use("/", indexRouter);
 
